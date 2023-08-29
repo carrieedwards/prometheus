858e989a8eabd71078ad90cdcb287bb101eaca7a
Fiona Liao <fiona.y.liao@gmail.com>
2023-08-29 18:46:24 +0100

    Add WAL replay test, found bug
    Signed-off-by: Fiona Liao <fiona.y.liao@gmail.com>

--- tsdb/head_wal.go
+++ tsdb/head_wal.go
@@ -731,6 +731,7 @@ func (h *Head) loadWBL(r *wlog.Reader, multiRef map[chunks.HeadSeriesRef]chunks.
 					return
 				}
 				decodedCh <- markers
+			//FIXME: add histogram sample types
 			default:
 				// Noop.
 			}
